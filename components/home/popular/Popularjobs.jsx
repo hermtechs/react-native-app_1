0c560b07a2e75e117dd61576c4335f704b576548
hermtechs <hermtechs@gmail.com>
2023-11-18 03:17:15 +0300

    designed welcome screen and added new components

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { SIZES,COLORS } from '../../../constants'
+// import PopularJobCard from "./components/common/cards/popular/popularjobcard"
+// import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 import styles from './popularjobs.style'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+
+const isLoading = true;
+const error = true
+const popularJobsData = [1,2,3,4,5,6]
+
 
 const Popularjobs = () => {
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Common issues</Text>
+        <TouchableOpacity style={styles.headerBtn}><Text>Show all</Text></TouchableOpacity>
+      </View>
+      <ScrollView horizontal contentContainerStyle={{columnGap:SIZES.small}}>
+        {popularJobsData.map(item=>{
+          return <TouchableOpacity><Text>{item}</Text></TouchableOpacity>
+        })}
+      </ScrollView>
+        {isLoading===true?<ActivityIndicator size="large" color={COLORS.tertiary}/>
+        ?error:
+        <Text>Something went wrong</Text>
+        :
+        popularJobsData.map(item=>{
+          return  <PopularJobCard/>
+        })
+       
+        
+        }
+
     </View>
   )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { View, Text, TextInput,TouchableOpacity,Image } from 'react-native'
-import { icons } from '../../../constants'
+import { router } from 'expo-router'
+import {React, useState } from 'react'
+import { View, Text, TextInput,TouchableOpacity,Image,ScrollView } from 'react-native'
+import { icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
+const jobTypes = ["Battery Change", "Car washing", "Flat Tire"]
 
 const Welcome = () => {
+
+const [activeJobType, setActiveJobType] = useState("Full-Time")
+
   return (
     <View>
   <View style={styles.container}>
     <View>
-      <Text style={styles.userName}>Hello Herbert</Text>
+      <Text style={styles.userName}>Hello Herbert,</Text>
       <Text style={styles.welcomeMessage}>Find your service</Text>
     </View>   
 <View style={styles.searchContainer}>
@@ -24,6 +29,20 @@ const Welcome = () => {
     <Image source={icons.search} style={styles.searchBtnImage}/> 
     </TouchableOpacity>
   </View>
+   < ScrollView contentContainerStyle={{columnGap:SIZES.small}} horizontal={true} style={{margin:10}}>
+      {jobTypes.map(item=>{
+        return <TouchableOpacity  style={styles.tab(activeJobType, item)}
+              onPress={()=>{
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+        >
+          <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+        </TouchableOpacity>
+      })
+
+      }
+    </ScrollView>
    </View>
  </View>
   )
